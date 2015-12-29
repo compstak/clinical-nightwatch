38f9c9ade503475c2f600593f5a8f89abf69f146
Alan Chusuei <alan@compstak.com>
2015-12-29 12:59:07 -0500

    fix missing param

--- package.js
+++ package.js
@@ -28,5 +28,5 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.addAssets('clinical:nightwatch-tests.js');
+  api.addAssets('clinical:nightwatch-tests.js', ['server']);
 });
